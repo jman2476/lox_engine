809bb502c3c8751e891bab8f1b7507523f3a4a9a
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-03-12 23:41:05 -0400

    add extract_move to game_writer.py

--- src/functions/game_writer.py
+++ src/functions/game_writer.py
@@ -3,21 +3,37 @@ import os, re
 
 # write and read PGN files
 def write_pgn(game, move=None, dir=None):
-   pass
+    pass
 
 def read_pgn(pgn_file, dir=None):
-   path = validate_path(pgn_file, dir)
-   reg = r'\n(1\.[\s\S]*){1}'
+    path = validate_path(pgn_file, dir)
+    reg = r'\n(1\.[\s\S]*){1}'
 
-   with open(path, 'r') as file:
-      contents = file
+    with open(path, 'r') as file:
+       contents = file
    
+    moves_block = re.match(reg, contents)
+    moves_string = remove_newline(moves_block)
+    moves = extract_moves(moves_string)
 
 
 def validate_path(file_path, dir=None):
-   path = file_path
-   if dir is not None and os.path.exists(dir):
-      path = os.path.join(dir, file_path)
-   if not os.path.exists(path):
-      raise Exception("Invalid path to PGN file")
-   return path
+    path = file_path
+    if dir is not None and os.path.exists(dir):
+        path = os.path.join(dir, file_path)
+    if not os.path.exists(path):
+        raise Exception("Invalid path to PGN file")
+    return path
+
+
+def extract_moves(moves_string):
+    split_moves = moves_string.split('.')
+    moves = []
+    for split in split_moves:
+        if len(split) == 1: continue
+        ply = split.split(' ')
+        moves.append(ply[1], ply[2]) # ply[1] -> white ply, ply[2] -> black ply
+    return moves
+
+def remove_newline(moves_block):
+    return ' '.join(moves_block.split('\n'))
