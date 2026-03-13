9188feb86929d0ed6dee0e6d4cf8c667dc6562f0
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-03-13 16:08:33 -0400

    add outline of evaluation

--- src/functions/evaluation.py
+++ src/functions/evaluation.py
@@ -18,7 +18,6 @@ def get_evaluation(board):
     w_pieces = board.white()
     b_pieces = board.black()
     eval = count_material(w_pieces, b_pieces)
-    
     return eval
 
 def count_material(w_pieces, b_pieces):
@@ -27,10 +26,37 @@ def count_material(w_pieces, b_pieces):
     for piece in w_pieces:
         if type(piece) is not King:
             w_material += piece._value
-
+            
     for piece in b_pieces:
         if type(piece) is not King:
             b_material += piece._value
 
     return w_material - b_material
      
+def calc_king_safety(board, side):
+    # For each square around the king, how many times is each attacked
+    # by enemy side minus how many times is the square defended?
+    # If squaes attacked, how many squares are free for the king to move into?
+    # From those free squares, how many more squares are free in the path?
+    pass
+
+def piece_activity(board, piece):
+    # What can this piece do if it could make multiple moves?
+    # Check 1,2,3,4,5 moves, piece that can be captured is value/10 points
+    # Each multi move calculated as capture._value/(10*moves)
+    # Return array
+    pass
+
+def pawn_structure(board, side):
+    # How many gaps are in the pawn structure?
+    # Do the pawns defend eachother?
+    # Are the pawns defended by other pieces?
+    # Are pawns stacked?
+    pass
+
+def space_control(board, side):
+    # How many squares on the other side of the board
+    #   can you attack? 
+    # Include squares occupied, empty squares, and 
+    #   enemy pieces attacked
+    pass
