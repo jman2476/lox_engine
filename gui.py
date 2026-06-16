36521b73df67af064b31639a819ea6a40dc3e96e
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-06-16 17:02:46 -0400

    add basic pygame window

--- gui.py
+++ gui.py
@@ -1,1 +1,21 @@
 import pygame
+
+# based on quick start from pygame.org/docs
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 800))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+    
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
