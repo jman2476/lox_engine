a9a4691810f7229b9cccaccaf81cdef94902ed22
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-06-16 17:19:24 -0400

    add placeholder game to gui

--- gui.py
+++ gui.py
@@ -6,6 +6,9 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 800))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() /2)
 
 while running:
     for event in pygame.event.get():
@@ -14,8 +17,21 @@ while running:
 
     screen.fill("purple")
     
+    # RENDER GAME HERE
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]: 
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
     pygame.display.flip()
 
-    clock.tick(60)
+    dt = clock.tick(60)/1000
 
 pygame.quit()
