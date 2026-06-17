3d786c0c25eff5d0db9fdfddd16ef5c686c08752
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-06-17 01:38:02 -0400

    add GUI_Board class

---
+++ src/graphics/board.py
@@ -0,0 +1,27 @@
+from src.board import Board
+from src.piece import (
+    Pawn, King,
+    Queen, Bishop,
+    Knight, Rook
+)
+from src.game import Game
+import pygame
+
+class GUI_Board(pygame.Surface):
+    def __init__(self):
+        self.game = Game()
+        self.board = {
+                "a":[None for i in range(0,8)],
+                "b":[None for i in range(0,8)],
+                "c":[None for i in range(0,8)],
+                "d":[None for i in range(0,8)],
+                "e":[None for i in range(0,8)],
+                "f":[None for i in range(0,8)],
+                "g":[None for i in range(0,8)],
+                "h":[None for i in range(0,8)],
+                }
+        self.ranks = [i for i in range(0,8)]
+        self.files = list("abcdefgh")
+
+    def set_squares(self):
+        pass
