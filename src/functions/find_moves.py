ad52c096346ed1251540c7e9aee04048a1f3b5c7
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-06-29 17:33:59 -0400

    ready for completing find knight, rook, queen, and bishop moves

--- src/functions/find_moves.py
+++ src/functions/find_moves.py
@@ -194,8 +194,8 @@ def find_queen_moves(game, queen):
 
 def find_rook_moves(game, rook):
     moves = []
-    side = rank.side
-    file, rank = rook.file, rook.rank
+    side = rook.side
+    file, rank = parse_square(rook.square())
     h_limits = game.board.bound_squares('horizontal')(file, rank, side)
     v_limits = game.board.bound_squares('vertical')(file, rank, side)
     print(f'Limits for {rook}: horizontal: {h_limits}; vertical: {v_limits}')
