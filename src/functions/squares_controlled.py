db836392186562a8be46f53d58c56941fb464895
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-07-08 02:51:31 -0400

    add square control functions for queen, bishop and rook

--- src/functions/squares_controlled.py
+++ src/functions/squares_controlled.py
@@ -5,7 +5,11 @@ from src.piece import (
     Queen, Bishop, 
     Knight, Rook
     )
-
+from src.functions.linears import (get_horizontal_squares,
+                                   get_vertical_squares)
+from src.functions.parse import (parse_square,
+                                 parse_square_reverse)
+from src.functions.diagonals import get_diagonal_squares
 class ControlledSquares():
     def __init__(self):
         self.squares = {}
@@ -53,12 +57,50 @@ def king_squares_controlled(board, king) -> list[str]:
 
 
 def queen_squares_controlled(board, queen) -> list[str]:
-    ...
+    squares = []
+    file, rank, side = queen.file, queen.rank, queen.side
+    h_edges = board.bound_squares('horizontal', True)(file, rank, side)
+    v_edges = board.bound_squares('vertical', True)(file, rank, side)
+    b_edges = board.bound_squares(
+        'diagonal', True)(file, rank, side, 'back')
+    f_edges = board.bound_squares(
+        'diagonal', True)(file, rank, side, 'forward')
+    
+    b_edge_sqs = [parse_square(sq) for sq in b_edges]
+    f_edge_sqs = [parse_square(sq) for sq in f_edges]
 
+    squares.extend([
+        *get_horizontal_squares(*h_edges),
+        *get_vertical_squares(*v_edges)
+    ])
 
-def bishop_squares_controlled(board, piece) -> list[str]:
-    ...
+    if b_edges[0] != b_edges[1]:
+        squares = get_diagonal_squares(b_edge_sqs[0], b_edge_sqs[1])
+        squares.extend(
+            [parse_square_reverse(sq) for sq in squares]
+        )
+    if f_edges[0] != f_edges[1]:
+        squares = get_diagonal_squares(f_edge_sqs[0], f_edge_sqs[1])
+        squares.extend(
+            [parse_square_reverse(sq) for sq in squares]
+        )
+    
+    return squares
 
+def bishop_squares_controlled(board, bishop) -> list[str]:
+    file, rank, side = bishop.file, bishop.rank, bishop.side
+    b_edges = board.bound_squares('diagonal', True)(file, rank, side, 'back')
+    f_edges = board.bound_squares('diagonal', True)(file, rank, side, 'forward')
+    b_edges_sq = [parse_square(sq) for sq in b_edges]
+    f_edges_sq = [parse_square(sq) for sq in f_edges]
+    b_squares, f_squares = [], []
+    if b_edges[0] !=  b_edges[1]:
+        b_squares = get_diagonal_squares(b_edges_sq[0], b_edges_sq[1])
+    if f_edges[0] != f_edges[1]:
+        f_squares = get_diagonal_squares(f_edges_sq[0], f_edges_sq[1])
+
+    return [parse_square_reverse(sq) 
+             for sq in  [*b_squares, *f_squares]]
 
 def knight_squares_controlled(board, knight) -> list[str]:
     return board.bound_squares('knight', True)(
@@ -68,5 +110,11 @@ def knight_squares_controlled(board, knight) -> list[str]:
     )
 
 
-def rook_squares_controlled(board, piece) -> list[str]:
-    ...
+def rook_squares_controlled(board, rook) -> list[str]:
+    file, rank, side = parse_square(rook.square()), rook.side
+
+    h_limits = board.bound_squares('horizontal', True)(file, rank, side)
+    v_limits = board.bound_squares('vertical', True)(file, rank, side)
+
+    return [*get_horizontal_squares(*h_limits),
+            *get_vertical_squares(*v_limits)]
