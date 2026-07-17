a661ee391b74c5bbab65d214f3d35b89b2aaa921
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-07-17 17:52:10 -0400

    add cleaner testing for test_king_safety, not currently working as expected

--- src/testing/test_evaluation.py
+++ src/testing/test_evaluation.py
@@ -20,3 +20,38 @@ class TestEvaluation(unittest.TestCase):
         print('King attacks: ')
         print(f'white: {k_safety[0].squares}')
         print(f'black: {k_safety[1].squares}')
+        
+        board.setup_by_fen('KQ6/8/8/8/8/2N5/6b1/k5b1 w - - 0 1')
+        for s in sides:
+            _, k_atk = space_control(board, s)
+            k_safety.append(k_atk)
+        
+        print('King attacks: ')
+        print(f'white: {k_safety[2].squares}')
+        print(f'black: {k_safety[3].squares}')
+
+        equality_asserts= [
+            {'b1': 2, 'b2': 2},
+            {'a7': 2},
+            {'b1': 4, 'b2': 2, 'a2': 2},
+            {'a7': 2,'a8': 2, 'b7': 2}
+        ]
+        
+        for i, k_s in enumerate(k_safety):
+            for key in k_s.squares:
+                self.assertTrue(key in equality_asserts[i])
+                print(f'expected: {key}: {equality_asserts[i][key]}')
+                print(f'real: {key}: {k_s.squares[key]}')
+                self.assertTrue(
+                    k_s.squares[key] == equality_asserts[i][key]
+                )
+
+        # self.assertEqual(k_safety[0].squares,
+        #                  {'b1': 2, 'b2': 2})
+        # self.assertEqual(k_safety[1].squares,
+        #                  {'a7': 2})
+        # self.assertEqual(k_safety[2].squares,
+        #                  {'b1': 4, 'b2': 2, 'a2': 2})
+        # self.assertEqual(k_safety[3].squares,
+        #                  {'a7': 2,'a8': 2, 'b7': 2})
+        
