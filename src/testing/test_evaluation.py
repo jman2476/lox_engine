d054bce6ffd9613c8b376c5cf1ef235ff0423c55
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-07-20 16:28:48 -0400

    update calculate king safety, and fix pgn parsing

--- src/testing/test_evaluation.py
+++ src/testing/test_evaluation.py
@@ -2,13 +2,15 @@ import unittest
 from src.functions.evaluation import (
     get_evaluation,
     count_material, 
-    space_control
+    space_control,
+    calc_king_safety
 )
 from src.board import Board
 from src.piece import *
 
 class TestEvaluation(unittest.TestCase):
     def test_king_safety(self):
+        print('-----Test King Safety-----')
         board = Board()
         board.setup_by_fen('KQ6/8/8/8/8/8/8/k5b1 w - - 0 1')
         sides = ['white', 'black']
@@ -48,3 +50,26 @@ class TestEvaluation(unittest.TestCase):
                     k_s.squares[key] == equality_asserts[i][key]
                 )
 
+    def test_calc_king_safety(self):
+        print('----Calc King Safety----')
+        board = Board()
+        board.setup_by_fen('KQ6/8/8/8/8/8/8/k5b1 w - - 0 1')
+        sides = ['white', 'black']
+        k_safety = []
+        for s in sides:
+            _, k_atk = space_control(board, s)
+            k_safety.append(k_atk)
+
+        print('King attacks: ')
+        print(f'white: {k_safety[0].squares}')
+        print(f'black: {k_safety[1].squares}')
+
+        computed_king_safety = []
+        for s in sides:
+            i = 0 if s == 'side' else 1
+            k_s_val = calc_king_safety(board, s, k_safety[i])
+            computed_king_safety.append(k_s_val)
+        
+        print('Computed King Safety')
+        print(f'white: {computed_king_safety[0]}')
+        print(f'black: {computed_king_safety[1]}')
