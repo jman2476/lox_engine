ecb87fccd2b0de5e682cb509b09ba548cde8f348
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-07-22 14:36:25 -0400

    add recursive depth search algorithm

--- src/functions/depth_search.py
+++ src/functions/depth_search.py
@@ -1,5 +1,6 @@
 from src.engines.naive import NaiveEngine
 from src.engines.engine import Engine
+import copy
 
 
 class DepthChart():
@@ -13,5 +14,29 @@ class DepthChart():
             depth_node = DepthChart(mv[0], mv[1])
             self.next.append(depth_node)
 
-def depth_search(engine:Engine, depth:int=3, moves:list[str]=[]) -> DepthChart:
-    ...
+def depth_search(engine:Engine, depth:int=3, breadth:int=5, level:int=0, moves:list[DepthChart]=[]) -> list[DepthChart]:
+    if depth <= level: return moves
+    if moves == []:
+        move_list = get_ranked_moves(engine)
+        for i, mv in enumerate(move_list):
+            if i >= breadth: break
+            moves.append(DepthChart(mv[0], mv[1]))
+    for mv in move_list:
+        engine_copy = copy.deepcopy(engine)
+        engine_copy.game.parse_move(mv.move)
+        ranked_moves = get_ranked_moves(engine_copy)
+        if len(ranked_moves) < breadth:
+            mv.set_next(ranked_moves)
+        else:
+            mv.set_next(ranked_moves[:breadth])
+        depth_search(engine_copy, depth, breadth, level+1, mv.next)
+    return moves
+
+
+
+def get_ranked_moves(engine:Engine)->list[tuple[str, float]]:
+    match engine:
+        case NaiveEngine():
+            return engine.rank_moves()
+        case _:
+            raise TypeError('Depth search: Unknown engine type')
