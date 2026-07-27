fb22d5194b537fe5f286b2a1bc6856a600b29928
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-07-27 16:54:23 -0400

    update pgnwriter to add event title

--- gui_depth_search_engines.py
+++ gui_depth_search_engines.py
@@ -24,7 +24,7 @@ def main():
     pygame.mouse.set_visible(True)
     screen = pygame.display.set_mode((1200, 900))
     # Depth search parameters
-    depth = 3
+    depth = 5
     breadth = 5
     multi_proc = False
     depth_mp = not multi_proc
