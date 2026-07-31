0045c3504f2a39d0f2490b0f12ad20b7bf8d7be9
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-07-31 18:21:40 -0400

    add base FastEngine class

---
+++ src/engines/fast_engine.py
@@ -0,0 +1,9 @@
+from src.engines.engine import Engine
+from src.game import Game
+from typing import Literal
+
+class FastEngine(Engine):
+    def __init__(self, game:Game, side:Literal['white', 'black'], search:tuple[int,int]=(2,2)):
+        super().__init__(game, side, 'fast', search[0])
+        self.breadth = search[1]
+        self.search = search
