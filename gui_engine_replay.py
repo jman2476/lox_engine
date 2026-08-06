d4f9db1e7a6a19b7d89fbffe5b760b4bac92b5f8
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-08-06 17:52:00 -0400

    update gui_engine_replay.py

--- gui_engine_replay.py
+++ gui_engine_replay.py
@@ -2,12 +2,10 @@ import datetime, time, logging, sys
 import pygame
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 import matplotlib.pyplot as plt
-import numpy as np
 from src.graphics.board import GUI_Board, Color
 from src.graphics.clock import Clock
 from src.graphics.error_box import ErrorBox
 from src.graphics.button import ExitButton, SetFenButton
-from src.engines.fool import FoolEngine
 from src.engines.naive import NaiveEngine
 from src.graphics.fen_box import FenBox
 from src.functions.game_writer import read_pgn
@@ -20,7 +18,6 @@ def main():
     running = True
     dt = 0
     elapsed = 0
-    last_move = 0
     game_board = GUI_Board()
     piece_font = pygame.font.Font("./fonts/nishiki-teki/NishikiTeki-MVxaJ.ttf", 30)
     error_box = ErrorBox()
@@ -48,8 +45,6 @@ def main():
 
     b_engine_d_t = []
     w_engine_d_t = []
-
-    logger = logging.getLogger(__name__)
 
     # FenBox
     fen_box = FenBox()
@@ -81,7 +76,6 @@ def main():
                 start = time.perf_counter_ns()
                 mv = engine_naive_w.play_move_mp_override(move_list[move_num][ply_num])
                 end = time.perf_counter_ns()
-                # logger.info(f'white move {mv} took {end - start}s')
                 w_engine_d_t.append(end - start)
                 print(game_board.game.board)
                 ply_num += 1
@@ -93,7 +87,6 @@ def main():
                 start = time.perf_counter_ns()
                 mv = engine_naive_b.play_move_override(move_list[move_num][ply_num])
                 end = time.perf_counter_ns()
-                # logger.info(f'black move {mv} took {end - start}s')
                 b_engine_d_t.append(end - start)
                 print(game_board.game.board)
                 ply_num += 1
@@ -121,10 +114,7 @@ def main():
         
         
         screen.blit(game_board, (50, 50))
-        # if move_piece is not None:
-        #     screen.blit(move_piece, (move_piece.x_pos, move_piece.y_pos))
         screen.blit(piece_font.render("Hello, chess. Time: %.3f, Turn: %s"%(elapsed, game_board.game.turn), 0, "black"), (10,10))
-        # screen.blit(piece_font.render("Fen: %s"%(game_board.game.fen), 0, "black"), (10,850))
         screen.blit(fen_box, (50, 855))
         screen.blit(fen_button, (950, 855))
         pygame.display.flip()
@@ -144,7 +134,6 @@ def main():
             plt.xticks(range(1,len(w_engine_d_t), 5))
             plt.autoscale(True, 'y')
             plt.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
-            # plt.axis((0, len(w_engine_d_t), min(b_engine_d_t), max(b_engine_d_t)))
             plt.minorticks_on()
             plt.suptitle(f'Naive Engine: Multi vs Single Process Move Time\nResult: {game_board.game.winner}\nFinal FEN: {game_board.game.fen}')
             print(f'Max time for black: {max(b_engine_d_t)}s')
