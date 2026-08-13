372d27c7093bf9e82acf1346f2a77093638be610
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-08-13 16:14:31 -0400

    add tests for FastEngine.eval_moves

--- src/testing/test_fastengine.py
+++ src/testing/test_fastengine.py
@@ -23,4 +23,37 @@ class TestFastEngine(unittest.TestCase):
         self.assertEqual(len(starting_moves), 20)
 
     def test_eval_moves(self):
-        ...
+        print("---Test Eval Moves---")
+        game = Game(dir='./test_games')
+        game.read_fen('k7/3Q4/K7/8/8/8/8/8 w - - 0 1')
+        engine = FastEngine(game, 'white', (2,3))
+        move_list = ['Qa7', 'Qb7', 'Qc7', 'Ka5']
+
+        move_evals = engine.eval_moves(move_list)
+        print(f'Evaluated moves: {move_evals}')
+
+        for mv in move_evals:
+            match mv[0]:
+                case 'Qa7':
+                    self.assertEqual(mv[1], 1000.0)
+                case 'Qb7':
+                    self.assertEqual(mv[1], 1000.0)
+                case 'Qc7':
+                    self.assertEqual(mv[1], 0.0)
+                case 'Ka5':
+                    self.assertTrue(mv[1] > 0)
+
+        engine.game.read_fen('K7/3q4/k7/8/8/8/8/8 b - - 0 1')
+        move_list = ['Qa7', 'Qb7', 'Qc7', 'Ka5']
+        move_evals = engine.eval_moves(move_list)
+        print(f'Evaluated moves: {move_evals}')
+        for mv in move_evals:
+            match mv[0]:
+                case 'Qa7':
+                    self.assertEqual(mv[1], -1000.0)
+                case 'Qb7':
+                    self.assertEqual(mv[1], -1000.0)
+                case 'Qc7':
+                    self.assertEqual(mv[1], 0.0)
+                case 'Ka5':
+                    self.assertTrue(mv[1] < 0)
