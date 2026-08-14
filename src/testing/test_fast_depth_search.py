458b1c4991a4a54e6c882112257fcf7fcc704a53
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-08-14 18:22:49 -0400

    expand fast_depth_search.depth_search function, not done yet

--- src/testing/test_fast_depth_search.py
+++ src/testing/test_fast_depth_search.py
@@ -3,3 +3,13 @@ from src.functions.fast_depth_search import (depth_search,
                                             search_process,
                                             get_best_move)
 from src.engines.fast_engine import FastEngine
+from src.game import Game
+
+class TestFastDepthSearch(unittest.TestCase):
+    def test_depth_search(self):
+        game = Game()
+        game.start_new_game()
+        engine = FastEngine(game, 'white', (2,3))
+
+        results = depth_search(engine)
+        print(results)
