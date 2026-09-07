9ede269477a23bb869ceff4af48e1cd80ccae469
Jeremy McKeegan <152367545+jman2476@users.noreply.github.com>
2026-09-07 18:08:12 -0400

    add profiler prints for cumulative time and total time for the top 30 functions

--- src/testing/profiling/fds_profile.py
+++ src/testing/profiling/fds_profile.py
@@ -1,7 +1,7 @@
 from src.game import Game
 from src.engines.fast_engine import FastEngine
 from src.functions.fast_depth_search import get_best_move
-import cProfile
+import cProfile, pstats
 import multiprocessing as mp
 
 
@@ -10,7 +10,17 @@ def main():
     game.start_new_game()
     engine = FastEngine(game, 'white', (2,3))
 
-    cProfile.run('get_best_move(engine)')
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    get_best_move(engine)
+
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats('cumtime')
+    stats.print_stats(30)
+
+    stats.sort_stats('tottime')
+    stats.print_stats(30)
 
 if __name__ == '__main__':
     mp.set_start_method('forkserver')
